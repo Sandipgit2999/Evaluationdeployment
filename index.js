bddfeee56c32d3ca91527b7b820e8b9be69d294e
Sandip <suryawanshisandip2999@gmail.com>
2023-03-14 17:12:13 +0530

    mongoose link change

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const { connection } = require("./config/db");
 const { authorization } = require("./middlewares/Authorization");
 const { UserController } = require("./routes/user.route");
 const { TodosController } = require("./routes/todo.route");
+const { videoController } = require("./routes/videos.route");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/user", UserController);
+app.use("/videos",videoController);
 // app.use(authorization);
 app.use("/todos", TodosController);
 
